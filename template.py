d65c8f86a29b040e91ad44384b72d8b54c20e30e
Caleb1998 <calebjamespatricksmit98@gmail.com>
2019-07-29 17:10:47 +0100

    counts up and loops list

--- template.py
+++ template.py
@@ -12,31 +12,32 @@ Date: 28/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
+import itertools
 GPIO.setmode(GPIO.BOARD)
 chan_list_out=[33,35,37]
 GPIO.setup(chan_list_out,GPIO.OUT)
 chan_list_in = [38,40]
 GPIO.setup(chan_list_in,GPIO.IN)
-
+binary=list(itertools.product(range(2),repeat=3))
+k=0
 # Logic that you write
 def main():
 #	GPIO.output(chan_list_out,1)
 #	time.sleep(1)
 #	GPIO.output(chan_list_out,0)
 #	time.sleep(1)
-	while GPIO.input(40) == 0:
-		GPIO.output(chan_list_out,0)
-		time.sleep(0.1)
-	while GPIO.input(40)==1:
-		GPIO.output(chan_list_out,1)
-		time.sleep(0.1)
-
+	global k	
+	print(binary[k])
+	time.sleep(1)
+	k+=1
+	if k==8:
+		k=0
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
         while True:
-            main()
+       		main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
