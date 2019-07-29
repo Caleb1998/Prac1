8b6f1302d2e354d48b72b8b70e09a27f891e9d54
Caleb1998 <calebjamespatricksmit98@gmail.com>
2019-07-29 16:05:26 +0100

    update python LED respond to switch

--- template.py
+++ template.py
@@ -15,13 +15,21 @@ import time
 GPIO.setmode(GPIO.BOARD)
 chan_list_out=[33,35,37]
 GPIO.setup(chan_list_out,GPIO.OUT)
+chan_list_in = [38,40]
+GPIO.setup(chan_list_in,GPIO.IN)
 
 # Logic that you write
 def main():
-	GPIO.output(chan_list_out,1)
-	time.sleep(1)
-	GPIO.output(chan_list_out,0)
-	time.sleep(1)
+#	GPIO.output(chan_list_out,1)
+#	time.sleep(1)
+#	GPIO.output(chan_list_out,0)
+#	time.sleep(1)
+	while GPIO.input(40) == 0:
+		GPIO.output(chan_list_out,0)
+		time.sleep(0.1)
+	while GPIO.input(40)==1:
+		GPIO.output(chan_list_out,1)
+		time.sleep(0.1)
 
 # Only run the functions if 
 if __name__ == "__main__":
